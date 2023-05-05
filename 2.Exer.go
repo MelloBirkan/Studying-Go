7db3e4f5f8f592d135b3ca0937cd75dd6b3255ea
MelloBirkan <82703050+MelloBirkan@users.noreply.github.com>
2023-05-05 19:43:45 +0000

    first exercise

---
+++ 2.Exer.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO
+	var language = "Go"
+	const realeased = 2009
+	isAProgramingLanguage := true
+
+	fmt.Println(language, realeased, isAProgramingLanguage)
+}
